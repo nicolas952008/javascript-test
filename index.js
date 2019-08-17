69c4f29122dd983c53bd7c0b660dc44e1b262f46
nicolas952008 <nickolas952008@gmail.com>
2019-08-17 18:41:56 -0500

    Finished Class

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hola');
+
+function calculate(){
+    console.log('Hola');
+    var l1 = Number(document.getElementById('lado1').value);
+    var l2 = Number(document.getElementById('lado2').value);
+    var ans = Math.sqrt(Math.pow(l1,2)+Math.pow(l2,2));
+    document.getElementById('ans-txt').innerHTML = ans;
+    
+}
